eb0f066a6ae65b7293e5d6189d8556cd0b081ad5
DctorWei1314 <61504793+DctorWei1314@users.noreply.github.com>
2020-06-08 15:17:31 +0800

    初始化工作。加了C3P0的jar包，配置了数据库池，建了一些存放资源的目录.eg(img,css)

---
+++ src/com/web/util/C3P0Demo.java
@@ -0,0 +1,39 @@
+package com.web.util;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+//用该类获得connection，释放Connection，PreparedStatement，ResultSet相关对象也不要自己close，交给它的静态方法，不要自己防范空指针
+public class C3P0Demo {
+    private static ComboPooledDataSource c3p0 = new ComboPooledDataSource("c3p0");;
+    public static Connection getConnection(){
+        try {
+            return c3p0.getConnection();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+    public static void close(Connection conn) throws SQLException {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                throw e;
+            }
+        }
+    }
+    public static void close(PreparedStatement pstate) throws SQLException {
+        if(pstate!=null){
+            pstate.close();
+        }
+    }
+    public static void close(ResultSet rs) throws SQLException {
+        if(rs!=null){
+            rs.close();
+        }
+    }
+}
